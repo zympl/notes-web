4d9daa8466d6e47e36b6c2173b3f305c7e698a49
Sinda_Mary <sindamary.sm@gmail.com>
2022-04-22 12:55:32 +0530

    Effects added to left-Nav-Icon

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function toggleLeftNav(){
-  const leftNav = document.getElementById('leftNav')
-  leftNav.classList.toggle("closed");
-  
+function toggleLeftNav(event) {
+    const leftNav = document.getElementById('leftNav')
+    leftNav.classList.toggle("closed");
+    event.target.classList.toggle("ri-menu-unfold-line");
 }
